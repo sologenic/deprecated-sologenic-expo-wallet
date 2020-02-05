ae860c1a695cd0846f407112247005002514a11d
Robert Creaner <robertcreaner@Stephs-iMac.hitronhub.home>
2020-02-04 16:54:52 -0800

    fix header height. success modal style, and add x address validation

--- constants/Layout.js
+++ constants/Layout.js
@@ -1,10 +1,10 @@
-import { Platform, Dimensions, PixelRatio } from 'react-native';
-const width = Dimensions.get('window').width;
-const height = Dimensions.get('window').height;
+import { Platform, Dimensions, PixelRatio } from "react-native";
+const width = Dimensions.get("window").width;
+const height = Dimensions.get("window").height;
 
-export const isIos = Platform.OS === 'ios';
-export const isAndroid = Platform.OS === 'android';
-export const screenWidth = Dimensions.get('screen').width;
-export const screenHeight = Dimensions.get('screen').height;
-export const headerHeight = (screenHeight / 120) * 10;
+export const isIos = Platform.OS === "ios";
+export const isAndroid = Platform.OS === "android";
+export const screenWidth = Dimensions.get("screen").width;
+export const screenHeight = Dimensions.get("screen").height;
+export const headerHeight = (screenHeight / 100) * 12;
 export const borderWidth = 1 / PixelRatio.get();
