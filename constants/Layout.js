e968c539aef9312307a05a59e3225af377bce2e1
Shingo Konnai <shingok@Shingos-iMac.hitronhub.home>
2020-02-04 16:54:03 -0800

    fix style issues

--- constants/Layout.js
+++ constants/Layout.js
@@ -6,5 +6,5 @@ export const isIos = Platform.OS === 'ios';
 export const isAndroid = Platform.OS === 'android';
 export const screenWidth = Dimensions.get('screen').width;
 export const screenHeight = Dimensions.get('screen').height;
-export const headerHeight = (screenHeight / 120) * 10;
+export const headerHeight = (screenHeight / 100) * 12;
 export const borderWidth = 1 / PixelRatio.get();
