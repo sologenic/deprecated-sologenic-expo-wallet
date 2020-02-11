07be859ebe25fe80f9b7071bbc785fbddcc7321a
Shingo Konnai <shingok@Shingos-iMac.hitronhub.home>
2020-02-11 10:38:44 -0800

    change app.cofig.js to prod

--- app.config.js
+++ app.config.js
@@ -1,9 +1,7 @@
 export default {
-  // server: "wss://s1.ripple.com",
-  server: "wss://testnet.xrpl-labs.com",
+  server: "wss://s1.ripple.com",
   soloHash: "534F4C4F00000000000000000000000000000000",
-  // soloIssuer: "rsoLo2S1kiGeCcn6hCUXVrCpGMWLrRrLZz",
-  soloIssuer: "rMiTTf8TA9co9Pmzuzy7bVBr1mTwXzmpyU",
+  soloIssuer: "rsoLo2S1kiGeCcn6hCUXVrCpGMWLrRrLZz",
   soloFlags: 131072,
   soloValue: "400000000",
   version: "1.0.3",
